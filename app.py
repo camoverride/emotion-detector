4beec89629adf181a1d66e908ec4ee103caac9a0
camoverride <camoverride@gmail.com>
2020-05-06 14:45:18 -0700

    add TODO note, change quote style, remove try/except block so errors arent swallowed

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
+"""
+TODO: one route should crop. Then the cropped face should be passed to every other route,
+where there is one route per model. That way each model can update without needing to wait
+for the others. Also: update some models (i.e. gender) less frequently.
+
+Set models to update only after previous HTTP request is processed and returned.
+
+Make sure that websockets aren't sending the same reply to every client... each client
+should access independently...
+"""
+
 from threading import Lock
 
 from flask import Flask, request, render_template
 from flask_socketio import SocketIO, emit
 
-# from face_utils import decode_image, crop_face, get_emotions
+from face_utils import decode_image, crop_face, get_emotions
 
-
-import random # for debugging only
 
 async_mode = None
 thread = None
@@ -20,24 +29,20 @@ def index():
     return render_template("index.html", async_mode=socketio.async_mode)
 
 
-@socketio.on('my_event', namespace='/emotion_detector')
+@socketio.on("my_event", namespace="/emotion_detector")
 def test_message(message):
     """
     The client-side javascript generates an event every 2 seconds, sending a video frame to
     this route. The frame is processed and an emotion is returned to the client.
     """
-    try:
-        # Decode the image.
-        image = decode_image(message["data"])
+    # Decode the image.
+    image = decode_image(message["data"])
 
-        # Crop the image to a single face.
-        cropped_face = crop_face(image)
+    # Crop the image to a single face.
+    cropped_face = crop_face(image)
 
-        # Get the category of the face.
-        emotion = get_emotions(cropped_face)
-
-    except:
-        emotion = random.choice(["lizzo", "lasagna", "yolo", "money", "super money", "matzah"])
+    # Get the category of the face.
+    emotion = get_emotions(cropped_face)
 
     # Send the category to the client.
     socketio.emit("emotion_response", {"data": emotion}, namespace="/emotion_detector")
