050be35abce7ab446f652739509edd2999bc87db
camoverride <camoverride@gmail.com>
2020-06-03 13:05:12 -0700

    remove $(document).ready(function ... beause it causes things to hang after first socket request is sent`

--- static/send_data_to_server.js
+++ static/send_data_to_server.js
@@ -1,17 +1,15 @@
 // Capture video frames from client, send them to the server, and recieves the response.
-$(document).ready(function() {
-    var socket = io("/compute_emotion_route");
+var socket = io("/compute_emotion_route");
 
-    // Sends video data to the server every 5 seconds.
-    window.setInterval(function() {
-        var capture = document.getElementById("video_canvas");
-        socket.emit("compute_emotion_event", {data: capture.toDataURL("image/jpeg")});
-    }, 5000);
+// Sends video data to the server every 5 seconds.
+window.setInterval(function() {
+    var capture = document.getElementById("video_canvas");
+    socket.emit("compute_emotion_event", {data: capture.toDataURL("image/jpeg")});
+}, 5000);
 
-    // Send response from server to client.
-    socket.on("emotion_model_response", function(msg, cb) {
-        $("#emotion_prediction").text(msg.data);
-        if (cb)
-            cb();
-    });
+// Send response from server to client.
+socket.on("emotion_model_response", function(msg, cb) {
+    $("#emotion_prediction").text(msg.data);
+    if (cb)
+        cb();
 });
